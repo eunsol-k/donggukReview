73ea776b1dbab7c4a5d18b718d5f45950715b42e
eunsol-k <solsol1203@gmail.com>
2024-08-22 14:32:32 +0900

    set react, springboot

---
+++ Back/donggukReview/src/main/java/com/donggukReview/donggukReview/DonggukReviewApplication.java
@@ -0,0 +1,13 @@
+package com.donggukReview.donggukReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonggukReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonggukReviewApplication.class, args);
+	}
+
+}
---
+++ Back/donggukReview/src/test/java/com/donggukReview/donggukReview/DonggukReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggukReview.donggukReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonggukReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
