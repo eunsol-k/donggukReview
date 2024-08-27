64b1fca7189aa75a3da9e2a2e753b90195857f7b
seoyoujin97 <seoyoujin97@gmail.com>
2024-08-27 15:46:02 +0900

    After merge and modify

--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import CategoryDisplay from '../components/CategoryDisplay';
+import LoginSection from '../components/LoginSection';
+import SearchSection from '../components/SearchSection';
+import UserProfile from '../components/UserProfile';
 
 function Home() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setLoggedInUser(null);
+  };
+
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      {/* 홈 페이지 내용 추가 */}
+      <Header />
+      <div style={styles.container}>
+        <div style={styles.section1}>
+          <CategoryDisplay />
+        </div>
+        <div style={styles.section2}>
+          <SearchSection />
+        </div>
+        <div style={styles.section3}>
+          {loggedInUser ? (
+            <UserProfile
+              nickname={loggedInUser.nickname}
+              image={loggedInUser.image}
+              likes={loggedInUser.likes}
+              reviews={loggedInUser.reviews}
+              onLogout={handleLogout}
+            />
+          ) : (
+            <LoginSection setLoggedInUser={setLoggedInUser} />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default Home;
